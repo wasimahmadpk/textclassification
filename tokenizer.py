e8c42f6ec05a5c3ef4107592ff67499458f3a694
Wasim <wasimahmad.qc@gmail.com>
2020-01-05 22:46:46 +0100

    added tokenizer and sentencizer

---
+++ tokenizer.py
@@ -0,0 +1,39 @@
+import spacy
+
+print('I am tokenizer')
+
+# Word tokenization
+spacy.load('en_core_web_sm')
+from spacy.lang.en import English
+
+
+# Load English tokenizer, tagger, parser, NER and word vectors
+nlp = English()
+
+text = """When learning data science, you shouldn't get discouraged!
+Challenges and setbacks aren't failures, they're just part of the journey. You've got this!"""
+
+#  "nlp" Object is used to create documents with linguistic annotations.
+my_doc = nlp(text)
+
+# Create list of word tokens
+token_list = []
+for token in my_doc:
+    token_list.append(token.text)
+print(token_list)
+
+# Create the pipeline 'sentencizer' component
+sbd = nlp.create_pipe('sentencizer')
+
+# Add the component to the pipeline
+nlp.add_pipe(sbd)
+
+
+#  "nlp" Object is used to create documents with linguistic annotations.
+doc = nlp(text)
+
+# create list of sentence tokens
+sents_list = []
+for sent in doc.sents:
+    sents_list.append(sent.text)
+print(sents_list)
